91feb70a8cb0f91157793a56a22087484bf0cebb
xylix <kerk.pelt@gmail.com>
2017-07-17 21:08:14 +0300

    automate vundle install

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 cd $HOME/dotfiles &&
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim &&
+vim +PluginInstall +qall
 ln -s $(pwd)/.gitconfig $HOME/.gitconfig &&
 ln -s $(pwd)/.vimrc $HOME/.vimrc &&
 curl https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim --create-dirs -o $HOME/.vim/colors/monokai.vim
