ede3fe870f781f2b984e6259fdaa15fa3d5fb039
xylix <kerk.pelt@gmail.com>
2017-07-17 21:06:57 +0300

    added vundle install to script, renamed it

--- init.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-cd $HOME/dotfiles
-ln -s $(pwd)/.gitconfig $HOME/.gitconfig
-ln -s $(pwd)/.vimrc $HOME/.vimrc
-curl https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim --create-dirs -o $HOME/.vim/colors/monokai.vim
