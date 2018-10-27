99d720b5fe51bc4a5368e029b37126eda8af8f49
kerkko Pelttari <kerk.pelt@gmail.com>
2018-10-27 10:06:52 +0300

    Create polybar toggling script

---
+++ polybar-toggler.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Check if xdo is installed
+xdo_path=$(which xdo)
+if [ $? -ne 0 ]; then
+    echo "Cannot find \`xdo\` command." 1>&2
+    exit 1
+fi
+# Obtain bar's window id
+id=$(xdo id -N "Polybar")
+# Toggle bar visibility
+if [ "1" = "$(cat /tmp/polyvar)" ]
+then
+    xdo hide -N "Polybar"
+    echo 0 > /tmp/polyvar
+else
+    xdo show -N "Polybar"
+    echo 1 > /tmp/polyvar
+fi
