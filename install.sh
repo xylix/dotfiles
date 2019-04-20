60f72b245f9f4376f2f85f9c4492af995684f758
Kerkko Pelttari <kerk.pelt@gmail.com>
2019-04-20 10:32:07 +0300

    Finish symlinking

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
 #/bin/bash
-ln -s $HOME/.gitconfig $HOME/.gitconfig;
+ln -s .gitconfig $HOME/.gitconfig;
+ln -s .xinitrc $HOME/.xinitrc
+
 mkdir -p $HOME/.config/i3
 mkdir -p $HOME/.config/i3status
+mkdir -p $HOME/.config/rofi/config
 ln -s config/i3/config $HOME/.config/i3/config
 ln -s config/i3status/config $HOME/.config/i3status/config
+ln -s config/rofi/config $HOME/.config/rofi/config
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-ln -s .zshrc $HOME/.zshrc
+ln -s config/zshrc $HOME/.zshrc
+ln -s config/.zprofile $HOME/.zprofile
+
 ln -s $HOME/.vimrc $HOME/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim &&
 vim +PluginInstall +qall
