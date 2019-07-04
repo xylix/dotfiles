cd462a66fea202c573172d2bbd3c8ddda4ad6cd8
Kerkko Pelttari <kerk.pelt@gmail.com>
2019-07-04 15:14:37 +0300

    Add cli support for anagramtester

--- bin/anagramtest.py
+++ bin/anagramtest.py
@@ -1,7 +1,11 @@
-import string
-word = input("Input the original word: ")
-anagram = input("Input the anagram: ")
-measurement = {}
+import string, sys
+if (len(sys.argv) == 1):
+    word = input("Input the original word: ")
+    anagram = input("Input the anagram: ")
+elif (len(sys.argv) == 3):
+    word = sys.argv[1]
+    anagram = sys.argv[2]
+measurement= {}
 measurement2 = {}
 for i in range(0, 26):
     measurement[i] = word.count(string.ascii_lowercase[i])
