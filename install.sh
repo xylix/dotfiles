a33d1f55c29b693e89dbc2d4a810044d26f387d2
Kerkko Pelttari <kerk.pelt@gmail.com>
2020-03-10 15:53:38 +0200

    Fix installation of zsh syntax highl

--- install.sh
+++ install.sh
@@ -10,4 +10,4 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +PlugInstall +qall
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
