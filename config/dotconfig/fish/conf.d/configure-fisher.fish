77576f02b9c429b384bcf2286efe98634a64f352
Kerkko Pelttari <kerk.pelt@gmail.com>
2020-04-05 18:00:49 +0300

    Split aliases etc into their own fish config file

---
+++ config/dotconfig/fish/conf.d/configure-fisher.fish
@@ -0,0 +1,11 @@
+set -g fisher_path $HOME/.fisher-packages
+set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
+set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
+for file in $fisher_path/conf.d/*.fish
+    builtin source $file 2> /dev/null
+end
+
+if not functions -q fisher
+    curl https://git.io/fisher --create-dirs -sLo $fisher_path/functions/fisher.fish
+    fish -c fisher
+end
