9321b535e60836c1e2a1f34644ada3c87eba700e
Kerkko Pelttari <kerk.pelt@gmail.com>
2020-04-05 17:42:42 +0300

    Make fisher manage itself at .fisher-packages

--- config/dotconfig/fish/config.fish
+++ config/dotconfig/fish/config.fish
@@ -8,9 +8,15 @@ set PATH $HOME/.cargo/bin $PATH
 set PATH $HOME/.poetry/bin $PATH
 set PATH $HOME/neovim/bin $PATH
 
+set -g fisher_path $HOME/.fisher-packages
+set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
+set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
+for file in $fisher_path/conf.d/*.fish
+    builtin source $file 2> /dev/null
+end
+
 if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    curl https://git.io/fisher --create-dirs -sLo $fisher_path/functions/fisher.fish
     fish -c fisher
 end
 
