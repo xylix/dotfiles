932263d3bcc0e23164d4b132f46b402db529efea
Kerkko Pelttari <kerk.pelt@gmail.com>
2020-06-18 10:49:44 +0300

    Remove zsh, update gitignore_global with personal notes.md

--- install.sh
+++ install.sh
@@ -7,5 +7,3 @@ cp -r --symbolic-link $DOTFILES/config/dotconfig/. $HOME/.config/.
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +PlugInstall +qall
-
-git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
