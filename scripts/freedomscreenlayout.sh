23a26bf03cfa24cdda3c792730590a1b204d5436
Kerkko Pelttari <kerkko@robocorp.com>
2021-02-15 21:25:19 +0200

    update desktop screenlayout, update i3config

--- scripts/freedomscreenlayout.sh
+++ scripts/freedomscreenlayout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-I-1 --off --output DP-1 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output DP-2 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-3 --off
+xrandr --output DVI-I-1 --off --output DP-1 --primary --mode 3840x2160 --pos 2560x0 --rotate normal --output DP-2 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-3 --off
