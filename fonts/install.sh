4e67087686fd09dec39872fc61cdadf38d234d80
Kerkko Pelttari <kerkko@robocorp.com>
2021-03-01 10:30:18 +0200

    Update i3 and topgrade config, add fonts install script

---
+++ fonts/install.sh
@@ -0,0 +1,5 @@
+sudo mkdir -p /usr/local/share/fonts
+sudo cp -r System\ San\ Fransisco\ Display/ /usr/local/share/fonts/
+sudo cp -r IBM_Plex_Mono/ /usr/local/share/fonts
+sudo cp -R IBM_Plex_Sans/ /usr/local/share/fonts/
+sudo cp -r Roboto/ /usr/local/share/fonts/
