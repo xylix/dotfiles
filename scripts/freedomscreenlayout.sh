e5630e539a1342ca7be06430c79a1eb4dda66177
Kerkko Pelttari <kerk.pelt@gmail.com>
2021-04-13 22:18:58 +0300

    Rotate DP-2

--- scripts/freedomscreenlayout.sh
+++ scripts/freedomscreenlayout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-4 --primary --mode 3840x2160 --pos 2560x0 --rotate normal --output DP-2 --mode 2560x1440 --pos 0x0 --rotate normal
+xrandr --output DP-4 --primary --mode 3840x2160 --pos 2560x0 --rotate normal --output DP-2 --mode 2560x1440 --pos 0x0 --rotate right
