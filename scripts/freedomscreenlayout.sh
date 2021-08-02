f0b74ce1b33a3276b34b391fe90c4c04578b9a1c
Kerkko Pelttari <kerk.pelt@gmail.com>
2021-08-02 18:01:23 +0300

    update freedomscreenlayout.sh

--- scripts/freedomscreenlayout.sh
+++ scripts/freedomscreenlayout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-4 --primary --mode 3840x2160 --pos 2560x0 --rotate normal --output DP-2 --mode 2560x1440 --pos 0x0 --rotate right
+xrandr --output DP-4 --primary --mode 3840x2160 --pos 3840x0 --rotate normal --output DP-2 --mode 3840x2160 --pos 0x0 --rotate normal
