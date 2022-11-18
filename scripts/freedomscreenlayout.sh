03264932e49b111e864a90906d092f18affb59f0
Kerkko Pelttari <kerk.pelt@gmail.com>
2022-11-18 19:38:42 +0200

    Update i3config and freedomscreenlayout.sh

--- scripts/freedomscreenlayout.sh
+++ scripts/freedomscreenlayout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-4 --primary --mode 3840x2160 --pos 3840x0 --rotate normal --output DP-2 --mode 3840x2160 --pos 0x0 --rotate normal
+xrandr --output HDMI-0 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DP-4 --mode 3840x2160 --pos 3840x0 --rotate normal
