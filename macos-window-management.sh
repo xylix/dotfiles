c433cd860543d80101e5bd5767b6d2aec9532a3a
Kerkko Pelttari <kerk.pelt@gmail.com>
2025-04-03 15:59:05 +0300

    Add Yabai and skhd configs for macos

---
+++ macos-window-management.sh
@@ -0,0 +1,6 @@
+# source: https://lilysthings.org/blog/tiling-macos/ + 
+
+brew install koekeishiya/formulae/skhd
+skhd --start-service
+# Disable SIP (partially) https://github.com/koekeishiya/yabai/wiki/Disabling-System-Integrity-Protection
+brew install yabai
