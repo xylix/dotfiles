2fb405f3532f0cec9405480f3e8fc5a4467ae30e
Kerkko Pelttari <kerk.pelt@gmail.com>
2025-04-06 20:15:01 +0300

    MacOS window manager config update + sketchybar

--- macos-window-management.sh
+++ macos-window-management.sh
@@ -4,3 +4,8 @@ brew install koekeishiya/formulae/skhd
 skhd --start-service
 # Disable SIP (partially) https://github.com/koekeishiya/yabai/wiki/Disabling-System-Integrity-Protection
 brew install yabai
+# load scripting addons etc.
+
+brew tap FelixKratz/formulae
+brew install sketchybar
+# and the other steps from https://felixkratz.github.io/SketchyBar/setup
