304bc32c30a709eb874ebbb10b5d92f494dd4783
Kerkko Pelttari <kerk.pelt@gmail.com>
2025-04-09 19:31:35 +0300

    Disable systemd service installs by default and change from +PlugInstall to +Lazy on initial neovim plugin install

--- install.sh
+++ install.sh
@@ -9,11 +9,9 @@ cp -r --symbolic-link $DOTFILES/config/home/ $HOME/.
 cp -r --symbolic-link $DOTFILES/config/dotconfig/ $HOME/.config/.
 sudo cp -r --symbolic-link $DOTFILES/config/etc /etc
 # Install my system wide services
-sudo cp -r --symbolic-link $DOTFILES/config/systemd/system/* /etc/systemd/system/
+# sudo cp -r --symbolic-link $DOTFILES/config/systemd/system/* /etc/systemd/system/
 
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-nvim +PlugInstall +qall
+nvim +Lazy
 # curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 echo "Install rust"
 echo "Install fd. Either apt install fd-find or brew install fd"
